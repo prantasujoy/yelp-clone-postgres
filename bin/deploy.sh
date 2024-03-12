be64fe0e0b99a37b877859dc19e3a4deada32c4d
sujoy <sujoydhar95@gmail.com>
2024-03-12 15:01:25 +0600

    deploy file changed

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,21 @@
 #! /bin/bash
 
-if [[ $1 = "prod" ||  $1  = "dev" ]] && [[ $2 = "down" || $2 = "up" ]];then
+if [[ $1 = "prod" ||  $1  = "dev" ]] && [[ $2 = "down" || $2 = "up" ]]; then
 
     cd ..
 
     fileEnv="docker-compose.${1}.yaml"
     downOrUp=$2
+    build=""
 
-    echo "Running docker compose -f docker-compose.yaml -f $fileEnv $downOrUp"
+    if [[ $# -eq 3 && $3 = "build" ]]; then
+        build="--build"
+    fi
 
-    docker compose -f docker-compose.yaml -f $fileEnv $downOrUp
+    echo "Running docker compose -f docker-compose.yaml -f $fileEnv $downOrUp $build"
+
+    docker compose -f docker-compose.yaml -f $fileEnv $downOrUp $build
 
 else
-    echo "Need to follow format ./deploy.sh prod|dev down|up"
+    echo "Need to follow format ./deploy.sh prod|dev down|up [build]"
 fi
-
-
